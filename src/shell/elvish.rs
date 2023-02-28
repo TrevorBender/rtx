82c07cf5e006181517c63faff5092dba37915fc9
Trevor Bender <Trevor.Bender@gmail.com>
2023-02-27 22:48:30 -0500

    elvish: export rtx fn from eval

--- src/shell/elvish.rs
+++ src/shell/elvish.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use indoc::formatdoc;
+use itertools::Itertools;
 
 use crate::shell::{is_dir_in_path, Shell};
 
@@ -14,37 +15,40 @@ impl Shell for Elvish {
         let status = if status { " --status" } else { "" };
         let mut out = String::new();
         if !is_dir_in_path(dir) {
-            out.push_str(&format!("set paths = [ {dir} $@paths ]\n", dir = dir.display()));
+            out.push_str(&format!(
+                "set paths = [ {dir} $@paths ]\n",
+                dir = dir.display()
+            ));
         }
-        // TODO: how to export a function from elvish eval?
-        //
-        //    fn rtx {{|@args|
-        //      var command = ""
-        //      if (> (count $args) 0) {{
-        //        set command = $args[0]
-        //      }}
-        //      if (or (eq $command "deactivate") (eq $command "shell")) {{
-        //        eval ({exe} $command $args | slurp)
-        //      }} else {{
-        //        {exe} $command $args
-        //      }}
-        //    }}
         out.push_str(&formatdoc! {r#"
             set-env RTX_SHELL elvish
 
-            fn _rtx_hook {{
+            edit:add-var rtx~ {{|@args|
+              var command
+              if (> (count $args) 0) {{
+                set command = $args[0]
+              }}
+              if (or (eq $command "deactivate") (eq $command "shell")) {{
+                eval ({exe} $@args | slurp)
+              }} else {{
+                {exe} $@args
+              }}
+            }}
+
+            fn rtx_hook {{
               eval ({exe} hook-env{status} -s elvish | slurp)
             }}
 
-            set edit:before-readline = [ $@edit:before-readline $_rtx_hook~ ]
-            _rtx_hook
+            set edit:before-readline = [ $@edit:before-readline $rtx_hook~ ]
+            rtx_hook
             "#});
 
         out
     }
 
     fn deactivate(&self, path: String) -> String {
-        self.set_env("PATH", &path)
+        let path = path.split(':').join(" ");
+        format!("set paths = [ {path} ]\n")
     }
 
     fn set_env(&self, k: &str, v: &str) -> String {
